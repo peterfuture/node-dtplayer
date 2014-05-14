75d42ca9cc6eead6f0f147ddf6dce99f923f4ff6
peter future <peter_future@outlook.com>
2014-05-14 11:09:18 +0800

    make video ex work

--- libs/video.js
+++ libs/video.js
@@ -5,6 +5,9 @@ var ffi = require('ffi');
 var Struct = require('ref-struct');
 
 var voidptr = ref.refType(ref.types.void);
+var int64_t = ref.types.int64;
+var intptr = ref.refType(ref.types.int);
+var uint8_ptr = ref.refType(ref.types.uint8);
 
 var EX_VO = Struct(
 {
@@ -23,14 +26,28 @@ var EX_VO = Struct(
 
 var pic_t = Struct(
 {
- 	data:voidptr,
-	linesize:voidptr,
-    pts:'int',
-	dts:'int',
-	duration:'int'
+    data0:uint8_ptr,
+    data1:uint8_ptr,
+    data2:uint8_ptr,
+    data3:uint8_ptr,
+    data4:uint8_ptr,
+    data5:uint8_ptr,
+    data6:uint8_ptr,
+    data7:uint8_ptr,
+    linesize0:'int',
+    linesize1:'int',
+    linesize2:'int',
+    linesize3:'int',
+    linesize4:'int',
+    linesize5:'int',
+    linesize6:'int',
+    linesize7:'int',
+    pts:int64_t,
+    dts:int64_t,
+    duration:'int'
 }
 );
-pic_ptr = ref.refType(pic_t);
+var picptr = ref.refType(pic_t);
 
 var vo_ex_init = ffi.Callback('int',[],function()
 {
@@ -44,7 +61,7 @@ var vo_ex_stop = ffi.Callback('int',[],function()
 	return 0;	
 });
 
-var vo_ex_render = ffi.Callback('int',[voidptr],function(pic)
+var vo_ex_render = ffi.Callback('int',[picptr],function(pic)
 {
 	console.log('nodejs- ex vo render one frame');
 	return 0;	
@@ -52,14 +69,18 @@ var vo_ex_render = ffi.Callback('int',[voidptr],function(pic)
 
 exports.getVO = function ()
 {
-	var vo = new EX_VO();
-	//var vo = ref.alloc(EX_VO)
-	vo.id = 0;
-	vo.name = 'ex vo';
-	vo.vo_init = vo_ex_init;
-	vo.vo_stop = vo_ex_stop;
-	vo_vo_render = vo_ex_render;
-	return vo;
+    var vo = new EX_VO();
+    //var vo = ref.alloc(EX_VO)
+    vo.id = 0;
+    vo.name = 'ex vo';
+    vo.vo_init = vo_ex_init;
+    vo.vo_stop = vo_ex_stop;
+    vo.vo_render = vo_ex_render;
+    
+    vo.handle = null;
+    vo.next = null;
+    vo.vo_priv = null;
+    return vo;
 }
 
 //test
@@ -67,13 +88,11 @@ exports.getVO = function ()
 var ex_vo = new EX_VO();
 ex_vo.id = 0;
 ex_vo.name = 'ex vo';
-ex_vo.vo_init = vo_ex_init;
 ex_vo.vo_stop = vo_ex_stop;
 ex_vo_vo_render = vo_ex_render;
 
 console.log(ex_vo.id);
 console.log(ex_vo.name);
-ex_vo.vo_init();
-ex_vo.vo_stop();
-ex_vo.vo_render();
-*/
+//ex_vo.vo_init();
+//ex_vo.vo_stop();
+//ex_vo.vo_render();*/
